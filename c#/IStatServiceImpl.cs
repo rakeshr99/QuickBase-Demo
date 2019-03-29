a5b95e34eb241370a9b5e097608f90709607f5f2
rakeshr99 <radhakrishnan.r@husky.neu.edu>
2019-03-29 16:08:46 -0400

    Initial commit with implementation

---
+++ c#/IStatServiceImpl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Backend
+{
+    class IStatServiceImpl : IStatService
+    {
+        public List<Tuple<string, int>> GetCountryPopulations()
+        {
+            List<Tuple<string, int>> list = new List<Tuple<string, int>>();
+            SQLiteConnection sQLiteConnection = null;
+
+            try
+            {
+                sQLiteConnection = new SQLiteConnection("Data Source=citystatecountry.db;Version=3;FailIfMissing=True");
+                sQLiteConnection.Open();
+                String sql = "select country.countryname as country, sum(city.population) as population from country country inner join state state on state.countryId = country.countryId inner join city city on city.stateId = state.stateId group by countryname";
+                SQLiteCommand command = new SQLiteCommand(sql, sQLiteConnection);
+                SQLiteDataReader sdr = command.ExecuteReader();
+                while (sdr.Read())
+                {
+                    //Console.WriteLine("Country :" + sdr["country"] + " Population :" + sdr["population"]);
+                    list.Add(new Tuple<string, int>((sdr["country"]).ToString(), Convert.ToInt32(sdr["population"])));
+
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            finally
+            {
+                sQLiteConnection.Close();
+            }
+
+            return list;
+        }
+
+        public Task<List<Tuple<string, int>>> GetCountryPopulationsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- c#/Program.cs
+++ c#/Program.cs
@@ -4,6 +4,7 @@ using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace Backend
 {
@@ -11,16 +12,62 @@ namespace Backend
     {
         static void Main(string[] args)
         {
+            const string USA = "United States of America";
             Console.WriteLine("Started");
             Console.WriteLine("Getting DB Connection...");
 
             IDbManager db = new SqliteDbManager();
             DbConnection conn = db.getConnection();
-
+            Console.WriteLine("hello world");
             if(conn == null)
             {
                 Console.WriteLine("Failed to get connection");
             }
+
+            IStatService statServiceFromDb = new IStatServiceImpl();
+            List<Tuple<string, int>> listFromDb = statServiceFromDb.GetCountryPopulations();
+
+            Console.WriteLine("results from the Database class ----------------------------------");
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (Tuple<string, int> tuple in listFromDb)
+            {
+                Console.WriteLine("Country :" + tuple.Item1 + " Population :" + tuple.Item2);
+                dict.Add(tuple.Item1, tuple.Item2);
+            }
+
+            IStatService statServiceFromConcrete = new ConcreteStatService();
+
+            List<Tuple<string, int>> listFromConcrete = statServiceFromConcrete.GetCountryPopulations();
+
+          
+            Console.WriteLine("results from the concrete class --------------------------------------");
+
+            foreach (Tuple<string, int> tuple in listFromConcrete)
+            {
+                Console.WriteLine("Country :" + tuple.Item1 + " Population :" + tuple.Item2);
+
+                if (tuple.Item1.Equals(USA))
+                {
+                    continue;
+                }
+                if (!dict.ContainsKey(tuple.Item1))
+                {
+                    dict.Add(tuple.Item1, tuple.Item2);
+                }
+                
+            }
+
+            Console.WriteLine("Total results and merging both lists --------------------------------------");
+            foreach(KeyValuePair<string, int> entry in dict)
+            {
+                Console.WriteLine("Country :" + entry.Key + " Population :" + entry.Value);
+            }
+
+            Console.WriteLine("Total results and merging both lists --------------------------------------size : " + dict.Count);
+
+            Console.ReadKey();
+
         }
     }
 }
